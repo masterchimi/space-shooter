7ed0efa037b8e615a76814a8010a45317b42ff8b
masterchimi <35302455+masterchimi@users.noreply.github.com>
2018-01-10 09:32:12 -0600

    project unity

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+	private Rigidbody rb;
+
+	void Awake ()
+	{
+		rb = GetComponent <Rigidbody> ();
+	}
+
+
+	void Start ()
+	{
+		rb.velocity = transform.forward * speed;
+	}
+}
---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+	public float tumble;
+	private Rigidbody rb;
+
+	void Awake ()
+	{
+		rb = GetComponent <Rigidbody> ();
+	}
+
+	void Start ()
+	{
+		rb.angularVelocity = Random.insideUnitSphere * tumble; 
+	}
+}
